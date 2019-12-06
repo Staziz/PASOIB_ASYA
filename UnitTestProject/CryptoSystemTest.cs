a8fd3eeeb7f2e86910108382fdb4c4ffe3c080d7
Станислав Клесарев <klesarevklesarevstas@gmail.com>
2019-12-06 21:30:36 +0300

    Добавлены тесты (3 из них и не должны работать, которые с преоразованием строк в байты через кодировки)

---
+++ UnitTestProject/CryptoSystemTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PASOIB_ASYA;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class CryptoSystemTest
+	{
+		readonly string testString = "TestTestTest\t012345+6798/*@#_Яша, яша, ты ишак";
+		[TestMethod]
+		public void EncryptDecrypt()
+		{
+			string key = Security.GetKey("SomeKey");
+			string initVector = Security.GetInitializationVector();
+
+			for (int i = 0; i < 10; i++)
+			{
+				key = Security.GetKey("SomeKey");
+				initVector = Security.GetInitializationVector();
+				string ciperText = Security.EncryptFileAES(testString, key, initVector);
+				string plainText = Security.DecryptFileAES(ciperText, key, initVector);
+				Assert.AreEqual(testString, plainText);
+			}
+		}
+
+		[TestMethod]
+		public void StringToBytesAndViceVersaWithUnicode()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				using (AesCryptoServiceProvider aesCryptoProvider = new AesCryptoServiceProvider())
+				{
+					string key = Encoding.Unicode.GetString(aesCryptoProvider.Key);
+					string initVector = Encoding.Unicode.GetString(aesCryptoProvider.IV);
+					Assert.IsTrue(aesCryptoProvider.Key.SequenceEqual(Encoding.Unicode.GetBytes(key)));
+					Assert.IsTrue(aesCryptoProvider.IV.SequenceEqual(Encoding.Unicode.GetBytes(initVector)));
+				}
+			}
+		}
+
+		[TestMethod]
+		public void StringToBytesAndViceVersaWithUTF8()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				using (AesCryptoServiceProvider aesCryptoProvider = new AesCryptoServiceProvider())
+				{
+					string key = Encoding.UTF8.GetString(aesCryptoProvider.Key);
+					string initVector = Encoding.UTF8.GetString(aesCryptoProvider.IV);
+					Assert.IsTrue(aesCryptoProvider.Key.SequenceEqual(Encoding.UTF8.GetBytes(key)));
+					Assert.IsTrue(aesCryptoProvider.IV.SequenceEqual(Encoding.UTF8.GetBytes(initVector)));
+				}
+			}
+		}
+
+		[TestMethod]
+		public void StringToBytesAndViceVersaWithUTF32()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				using (AesCryptoServiceProvider aesCryptoProvider = new AesCryptoServiceProvider())
+				{
+					string key = Encoding.UTF32.GetString(aesCryptoProvider.Key);
+					string initVector = Encoding.UTF32.GetString(aesCryptoProvider.IV);
+					Assert.IsTrue(aesCryptoProvider.Key.SequenceEqual(Encoding.UTF32.GetBytes(key)));
+					Assert.IsTrue(aesCryptoProvider.IV.SequenceEqual(Encoding.UTF32.GetBytes(initVector)));
+				}
+			}
+		}
+
+		[TestMethod]
+		public void StringToBytesAndViceVersaWithBase64()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				using (AesCryptoServiceProvider aesCryptoProvider = new AesCryptoServiceProvider())
+				{
+					string key = Convert.ToBase64String(aesCryptoProvider.Key);
+					string initVector = Convert.ToBase64String(aesCryptoProvider.IV);
+					Assert.IsTrue(aesCryptoProvider.Key.SequenceEqual(Convert.FromBase64String(key)));
+					Assert.IsTrue(aesCryptoProvider.IV.SequenceEqual(Convert.FromBase64String(initVector)));
+				}
+			}
+		}
+
+		[TestMethod]
+		public void HashCompare()
+		{
+			string origHash = Security.GetMd5Hash(testString);
+			Assert.IsTrue(Security.IsStringEqualsHash(testString, origHash));
+		}
+	}
+}
---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a2d0c481-8dd0-4ef0-ad68-8a89c7b466c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
