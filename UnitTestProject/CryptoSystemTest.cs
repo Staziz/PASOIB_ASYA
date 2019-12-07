896f7bb5b17dad84065e533368824ce1033b45aa
Станислав Клесарев <klesarevklesarevstas@gmail.com>
2019-12-07 15:16:56 +0300

    Убраны ненужные тесты, теперь всё должно быть зелёненьким

--- UnitTestProject/CryptoSystemTest.cs
+++ UnitTestProject/CryptoSystemTest.cs
@@ -28,51 +28,6 @@ namespace UnitTestProject
 		}
 
 		[TestMethod]
-		public void StringToBytesAndViceVersaWithUnicode()
-		{
-			for (int i = 0; i < 10; i++)
-			{
-				using (AesCryptoServiceProvider aesCryptoProvider = new AesCryptoServiceProvider())
-				{
-					string key = Encoding.Unicode.GetString(aesCryptoProvider.Key);
-					string initVector = Encoding.Unicode.GetString(aesCryptoProvider.IV);
-					Assert.IsTrue(aesCryptoProvider.Key.SequenceEqual(Encoding.Unicode.GetBytes(key)));
-					Assert.IsTrue(aesCryptoProvider.IV.SequenceEqual(Encoding.Unicode.GetBytes(initVector)));
-				}
-			}
-		}
-
-		[TestMethod]
-		public void StringToBytesAndViceVersaWithUTF8()
-		{
-			for (int i = 0; i < 10; i++)
-			{
-				using (AesCryptoServiceProvider aesCryptoProvider = new AesCryptoServiceProvider())
-				{
-					string key = Encoding.UTF8.GetString(aesCryptoProvider.Key);
-					string initVector = Encoding.UTF8.GetString(aesCryptoProvider.IV);
-					Assert.IsTrue(aesCryptoProvider.Key.SequenceEqual(Encoding.UTF8.GetBytes(key)));
-					Assert.IsTrue(aesCryptoProvider.IV.SequenceEqual(Encoding.UTF8.GetBytes(initVector)));
-				}
-			}
-		}
-
-		[TestMethod]
-		public void StringToBytesAndViceVersaWithUTF32()
-		{
-			for (int i = 0; i < 10; i++)
-			{
-				using (AesCryptoServiceProvider aesCryptoProvider = new AesCryptoServiceProvider())
-				{
-					string key = Encoding.UTF32.GetString(aesCryptoProvider.Key);
-					string initVector = Encoding.UTF32.GetString(aesCryptoProvider.IV);
-					Assert.IsTrue(aesCryptoProvider.Key.SequenceEqual(Encoding.UTF32.GetBytes(key)));
-					Assert.IsTrue(aesCryptoProvider.IV.SequenceEqual(Encoding.UTF32.GetBytes(initVector)));
-				}
-			}
-		}
-
-		[TestMethod]
 		public void StringToBytesAndViceVersaWithBase64()
 		{
 			for (int i = 0; i < 10; i++)
